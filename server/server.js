10a7896bc966d595b723f276a85c43e01a088f09
Daniel Ndanema <51344937+JohnSorba@users.noreply.github.com>
2025-05-01 14:42:05 +0100

    minor update

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 // Define a Route
 app.get("/", (req, res) => {
   const data = { message: "Hello from the API from our Student Portal!" };
-  res.json(data);
+  res.json(data.message);
   // res.send("<h1>Hello, ES6 Express Server Side!</h1>");
 });
 
